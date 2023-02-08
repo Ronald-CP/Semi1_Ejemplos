a5123c0043f47350b4102391e9a7ebd16f8a3270
Ronald-CP <3004377060101@ingenieria.usac.edu.gt>
2023-02-07 23:41:20 -0600

    [ADD] Endpoints AWS s3

--- Servidor/index.js
+++ Servidor/index.js
@@ -10,10 +10,66 @@ app.use(cors(corsOptions))
 app.use(bodyParser.json({ limit: '10mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
-app.get('/', function (req, res) {
-  res.json({ messaje: 'Hola Seminario' })
-})
-
 var port = 4000
 app.listen(port)
 console.log('Escuchando en el puerto', port)
+
+//Se inicializa el sdk para menejar los servicios de AWS
+var AWS = require('aws-sdk')
+
+//*********************************************ALMACENAMIENTO****************************************************
+// ruta que se usa para subir una foto
+
+app.post('/subirfoto', function (req, res) {
+  var id = req.body.id
+  var foto = req.body.foto
+  //carpeta y nombre que quieran darle a la imagen
+
+  var cadena = 'fotos/' + id + '.jpg' // fotos -> se llama la carpeta UBICACION
+  //se convierte la base64 a bytes
+  let buff = new Buffer.from(foto, 'base64')
+
+  AWS.config.update({
+    region: 'us-east-1', // se coloca la region del bucket
+    accessKeyId: '',
+    secretAccessKey: '',
+  })
+
+  var s3 = new AWS.S3() // se crea una variable que pueda tener acceso a las caracteristicas de S3
+
+  const params = {
+    Bucket: 'semi1test', // nombre
+    Key: cadena, // Nombre de ubicacion
+    Body: buff, // Imagen enn bytes
+    ContentType: 'image', // tipo de contenido
+  }
+  const putResult = s3.putObject(params).promise()
+  res.json({ mensaje: putResult, status: true })
+})
+
+app.post('/obtenerfoto', function (req, res) {
+  var id = req.body.id
+  var cadena = 'fotos/' + id + '.jpg'
+
+  AWS.config.update({
+    region: 'us-east-1', // se coloca la region del bucket
+    accessKeyId: '',
+    secretAccessKey: '',
+  })
+
+  var S3 = new AWS.S3()
+
+  var getParams = {
+    Bucket: 'semi1test',
+    Key: cadena,
+  }
+
+  S3.getObject(getParams, function (err, data) {
+    if (err) {
+      res.json(err)
+    } else {
+      var dataBase64 = Buffer.from(data.Body).toString('base64') //resgresar de byte a base
+      res.json({ mensaje: dataBase64 })
+    }
+  })
+})
