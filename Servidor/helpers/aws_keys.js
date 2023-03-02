d421088504c5d8906901b164d89007c1b77b4836
Ronald-CP <3004377060101@ingenieria.usac.edu.gt>
2023-03-01 23:52:07 -0600

    [ADD]

--- Servidor/helpers/aws_keys.js
+++ Servidor/helpers/aws_keys.js
@@ -1,18 +1,18 @@
 let aws_keys = {
   s3: {
     region: 'us-east-1',
-    accessKeyId: 'AKIA4MQPRAC6Q5Y5W2VC',
-    secretAccessKey: '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',
+    accessKeyId: '',
+    secretAccessKey: '',
   },
   dynamodb: {
     region: 'us-east-2',
-    accessKeyId: 'AKIA4MQPRAC62R7QZSCC',
-    secretAccessKey: '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',
+    accessKeyId: '',
+    secretAccessKey: '',
   },
   rekognition: {
     region: 'us-east-1',
-    accessKeyId: 'AKIA4MQPRAC6X5VVNR62',
-    secretAccessKey: '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',
+    accessKeyId: '',
+    secretAccessKey: '',
   },
   translate: {
     region: '',
